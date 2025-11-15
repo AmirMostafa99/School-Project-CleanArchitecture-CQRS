8b0e8165c917d6c209c8117f4e6f9eb1b7724563
AmirMostafa99 <am7870878@gmail.com>
2025-11-15 22:01:42 +0200

    Add GetUserById Endpoint

---
+++ SchoolProjectCleanArchitecture/SchoolProject.Core/Features/ApplicationUser/Queries/Models/GetUserPaginationQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SchoolProject.Core.Features.ApplicationUser.Queries.Results;
+using SchoolProject.Core.Wrappers;
+
+namespace SchoolProject.Core.Features.ApplicationUser.Queries.Models
+{
+    public class GetUserPaginationQuery : IRequest<PaginatedResult<GetUserPaginationReponse>>
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+    }
+}
